7d2f555ee4c7e633843a1b764974160eb0d127c5
Peter Cai <peter@typeblog.net>
2021-06-10 18:45:51 +0800

    main: use NUM_MOTORS everywhere

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main() {
     digitalWrite(8, LOW);
 
     // Initialize the controllers
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < NUM_MOTORS; i++) {
         motors[i].Init();
     }
 
